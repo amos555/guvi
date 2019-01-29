bf4093377196d2dd8daeeb380eaac44394453ace
amos555 <46369662+amos555@users.noreply.github.com>
2019-01-29 04:59:18 -0800

    Update oddinterval.py

--- oddinterval.py
+++ oddinterval.py
@@ -1,5 +1,5 @@
-lower_limit = int(raw_input())
-upper_limit = int(raw_input())
-for i in range(lower_limit,upper_limit+1):
+lower_limit1 = int(raw_input())
+upper_limit2 = int(raw_input())
+for i in range(lower_limit1,upper_limit2+1):
   if(i%2 != 0):
     print(i)
