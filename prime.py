cc24d0352e147aba1fd67f0374f256d7dd066c37
amos555 <46369662+amos555@users.noreply.github.com>
2019-01-29 04:37:26 -0800

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,7 +1,7 @@
-num = int(raw_input())
-if num > 1:
-   for i in range(2,num):
-       if (num % i) == 0:
+number = int(raw_input())
+if number > 1:
+   for i in range(2,number):
+       if (number % i) == 0:
            print("no")
    else:
        print("yes")
