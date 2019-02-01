2bc3b5e93f3c449dba89e93c15adacc409c924dd
amos555 <46369662+amos555@users.noreply.github.com>
2019-02-01 10:02:32 +0530

    Update primeinterval.py

--- primeinterval.py
+++ primeinterval.py
@@ -1,6 +1,7 @@
-start = int(raw_input())
-end = int(raw_input())
-for val in range(start, end + 1): 
+usr = int(raw_input())
+usr = usr.split()
+temp = 1
+for val in range(int(usr[0]+1), int(usr[1])): 
    if val > 1: 
        for n in range(2, val): 
            if (val % n) == 0: 
