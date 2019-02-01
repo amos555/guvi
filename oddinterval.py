dad3900df8cdfc0cf53c2b97c0cf28d3738377d3
amos555 <46369662+amos555@users.noreply.github.com>
2019-02-01 09:35:28 +0530

    Update oddinterval.py

--- oddinterval.py
+++ oddinterval.py
@@ -1,7 +1,7 @@
-usr = (raw_input())
-usr = usr.split()
+usr1 = (raw_input())
+usr1 = usr1.split()
 temp =1
-for i in range(int(usr[0]) +1, int(usr[1])):
+for i in range(int(usr1[0]) +1, int(usr1[1])):
   if(i%2 != 0):
   	if(temp !=1):
   		print(" ")
