bdb5f5e0da8e430f89bfeeea3d589dd78b3fef70
amos555 <46369662+amos555@users.noreply.github.com>
2019-03-30 12:36:24 +0530

    Create removing_vowels.py

---
+++ removing_vowels.py
@@ -0,0 +1,15 @@
+s = raw_input()
+n = s
+
+vowels = ('a', 'e', 'i', 'o', 'u')
+for x in s.lower():
+	if x in vowels:
+		n = n.replace(x,"")
+		
+def rev(n):
+  str = ""
+  for i in n:
+    str = i + str
+  return str
+
+print (rev(n))
